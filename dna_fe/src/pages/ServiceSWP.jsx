2bacac4245745401754f04eb9cb681e486805ad4
Luu Tien Anh <anhltse184783@fpt.edu.vn>
2025-08-06 18:48:45 +0700

    last done

---
+++ dna_fe/src/pages/ServiceSWP.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { getAllServices } from '../api/serviceApi';
+
+const ServiceList = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            const services = await getAllServices();
+            setServices(services);
+        };
+        fetchServices();
+    }, []);
+
+    return (
+     
+        <div className="service-grid">
+        
+          {services.map((service) => (
+            <div key={service.serviceID}>
+              <h3 >{service.serviceName}</h3>
+                <p >{service.describe || 'Không có mô tả chi tiết.'}</p>
+              </div>
+          ))}
+     </div>
+ 
+       
+    )
+    
+ };
+  export default ServiceList ;
